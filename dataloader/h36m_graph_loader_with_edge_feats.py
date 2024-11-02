000382ec7a956e49419deef845fcbf8716cb77b5
Parashara <parasharar@gmail.com>
2024-11-02 14:03:14 +0800

    going to test GAT

---
+++ dataloader/h36m_graph_loader_with_edge_feats.py
@@ -0,0 +1,48 @@
+# Convert Human 3.6M dataset to GNN friendly input
+from dgl.data import DGLDataset
+import numpy as np
+import dgl
+import torch
+
+class Human36MGraphEdgeDataset(DGLDataset):
+    def __init__(self, two_dim_dataset_path, three_dim_dataset_path, label_dataset_path):
+        self.two_dim_dataset_path = two_dim_dataset_path
+        self.three_dim_dataset_path = three_dim_dataset_path
+        self.label_dataset_path  = label_dataset_path
+        self.two_dim_data = None
+        self.three_dim_data = None
+        self.labels = None
+        self.unique_labels = None
+        super().__init__(name="human_3.6m")
+        
+    def process(self):
+        # Datasets
+        self.two_dim_data = np.load(self.two_dim_dataset_path)
+        self.three_dim_data = np.load(self.three_dim_dataset_path)
+        self.labels = np.load(self.label_dataset_path)
+        unique_labels, tags = np.unique(self.labels, return_inverse=True)
+        self.unique_labels = unique_labels
+        self.labels = tags
+        assert len(self.two_dim_data) == len(self.three_dim_data) == len(self.labels)
+        
+    def __getitem__(self, index):
+        # Data
+        two_dim_data, three_dim_data, label = self.two_dim_data[index], self.three_dim_data[index], self.labels[index]
+        # Step 1: Define Graph
+        # Edge Connections [Source & Destination] <-- Human Body Structure
+        human_pose_edge_src = torch.LongTensor([0, 0, 0, 1, 2, 4, 5, 7, 8, 8, 8, 10, 11, 13, 14])
+        human_pose_edge_dst = torch.LongTensor([1, 4, 7, 2, 3, 5, 6, 8, 10, 13, 9, 11, 12, 14, 15])
+        graph = dgl.graph((human_pose_edge_src, human_pose_edge_dst))
+        graph = dgl.to_bidirected(graph)
+        # Add node features
+        graph.ndata['feat_2d'] = torch.Tensor(two_dim_data)
+        graph.ndata['feat_3d'] = torch.Tensor(three_dim_data)
+        # Add edge features
+        src_pos = graph.ndata['feat_3d'][human_pose_edge_src]
+        dst_pos = graph.ndata['feat_3d'][human_pose_edge_dst]
+        edge_distances = torch.norm(src_pos - dst_pos, dim=1)
+        graph.edata['feat'] = edge_distances
+        return [graph, label]
+        
+    def __len__(self):
+        return len(self.two_dim_data)
---
+++ models/simple_pose_gat.py
@@ -0,0 +1,65 @@
+import dgl
+import dgl.nn as dglnn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Simple GNN Model
+class SimplePoseGAT(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_size, num_classes):
+        self.input_dim = input_dim
+        #TODO.x try adding positional embedding
+        # self.input_dim = input_dim + 2 # in case we want to incorporate positional encoding (x,y)
+        self.output_dim = output_dim
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+
+        super(SimplePoseGAT, self).__init__()
+        self.gat_layers = nn.ModuleList()
+
+        # two-layer GAT with heads (8,1)
+        self.heads = [8,1]
+        self.gat_layers.append(
+            dglnn.GATConv(
+                self.input_dim,
+                self.hidden_size,
+                self.heads[0],
+                feat_drop=0.6,
+                attn_drop=0.6,
+                activation=F.elu,
+            )
+        )
+        self.gat_layers.append(
+            dglnn.GATConv(
+                self.hidden_size * self.heads[0],
+                self.output_dim,
+                self.heads[1],
+                feat_drop=0.6,
+                attn_drop=0.6,
+                activation=None,
+            )
+        )
+        self.classifier = nn.Linear(output_dim, num_classes)
+
+
+    def forward(self, graph, node_2d_features):
+        #TODO.x in case we want positional encoding
+        # Concatenate 2D coordinates as positional embeddings
+        # pos_embeddings = graph.ndata['feat_2d']  # (x, y) coordinates
+        # h = torch.cat([node_2d_features, pos_embeddings], dim=1)  # Concatenate along feature dimension
+
+        h = node_2d_features
+        for i, layer in enumerate(self.gat_layers):
+            h = layer(graph, h)
+            if i == len(self.gat_layers) - 1:  # last layer
+                h = h.mean(1)
+            else:  # other layer(s)
+                h = h.flatten(1)
+
+        # Perform classification
+        graph.ndata['h'] = h
+        y = dgl.mean_nodes(graph, 'h')
+        label = self.classifier(y)
+
+        #return 3d and label outputs
+        return h, label
